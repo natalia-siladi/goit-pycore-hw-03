7e564f4f73de9fbc93f0d47b0fc40f8a92ae9ad6
Nataliia Siladi <nataliiasiladi@Nataliias-MBP.fritz.box>
2024-06-20 00:18:35 +0200

    added task 1 without return

---
+++ get_days.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+def get_days_from_today(date):
+
+# create today date
+  date_today = datetime.today().date()
+ 
+# create input date, exeptions
+  try:
+    input_date = datetime.strptime(date, "%Y.%m.%d").date()
+
+  except ValueError: 
+    print("Wrong date forrmat. Try to use 'YYYY.MM.DD'")
+  
+# count difference
+  delta =  date_today - input_date
+  #print (delta.days)
+  print(delta.days)
+  
+
+get_days_from_today("2012.10.11")
+  
+
+
+# why I can't see in terminal if I put return, only print
+
+
+
+    
